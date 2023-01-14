8eee91a55ee333f8a5337905e4b8d58f092ed822
nasimit202 <nasimitjesbd@gmail.com>
2023-01-14 17:46:05 +0600

    header complete

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Slider from './Slider';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Slider></Slider>
+        </div>
+    );
+};
+
+export default Home;
