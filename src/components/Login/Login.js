7faa532290626060a19b0f87bd156d79f35be5d1
nasimit202 <nasimitjesbd@gmail.com>
2023-01-16 17:42:16 +0600

    login page design

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Google from './../../img/google.svg'
 
 const Login = () => {
     return (
         <div className=''>
-            <div>
-                <span><FontAwesomeIcon icon="fa-brands fa-google" /></span>
-                <p>Google</p>
+            <div className='border' style={{width: '400px', height: '160px', margin: '0 auto', textAlign: 'center', borderRadius: '5px', marginTop: '150px', justifyContent: 'center'}}>
+                <button className='btn btn-primary mt-5 text-light'><span className='pe-3'><img src={Google} alt='Google logo' style={{width: '25px',}}/></span>Login with Google</button>
             </div>
         </div>
     );
