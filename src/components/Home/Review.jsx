8bd51b1bb32d40b327f00b8aef52a366e26eee60
Md. Nasim Reza <nasimitjesbd@gmail.com>
2023-05-21 11:53:50 +0600

    review section down

--- src/components/Home/Review.jsx
+++ src/components/Home/Review.jsx
@@ -1,44 +1,59 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import Awal from '../../assets/images/awal.jpg'
+import Customer1 from '../../assets/images/customer1.jpg'
+import Customer2 from '../../assets/images/customer2.jpg'
+import Customer3 from '../../assets/images/customer3.jpg'
+import Customer4 from '../../assets/images/customer4.jpg'
 const reviewSection = [
-    { name: 'Jhankar', img: Awal },
-    { name: 'Mahabub', img: 'https://www.facebook.com/photo/?fbid=8027606530646322&set=a.104886309585090' },
-    { name: 'Tamima', img: 'https://www.facebook.com/photo/?fbid=8027606530646322&set=a.104886309585090' },
-    { name: 'Nasir', img: 'https://www.facebook.com/photo/?fbid=8027606530646322&set=a.104886309585090' },
-    { name: 'Sakib', img: 'https://www.facebook.com/photo/?fbid=8027606530646322&set=a.104886309585090' },
-    { name: 'Sisir', img: 'https://www.facebook.com/photo/?fbid=8027606530646322&set=a.104886309585090' },
+    { name: 'Loren Austin', designation: 'IT Consultant', img: Customer1 },
+    { name: 'Jengina Paku', designation: 'Business Man', img: Customer2 },
+    { name: 'Pawlo Piko', designation: 'House Wife', img: Customer3 },
+    { name: 'Anisa Wastin', designation: 'CEO', img: Customer4 }
 
 ]
+import { Autoplay } from "swiper";
+
 const Review = () => {
     return (
         <div className='lg:px-32 sm:px-12 py-12'>
+            <p>--- SOME HAPPY FACES</p>
+            <h2 className='text-3xl font-semibold pb-12 pt-5'>Real Happy Customers, Real Stories</h2>
             <Swiper
 
                 spaceBetween={50}
                 slidesPerView={1}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: true,
+                }}
+                loop={true}
+                modules={[Autoplay]}
                 onSlideChange={() => console.log('slide change')}
                 onSwiper={(swiper) => console.log(swiper)}
             >
+
                 {
                     reviewSection.map(rev => (
                         <SwiperSlide>
-                            <div className="card w-96 bg-base-100 shadow-xl mx-auto">
-                                <figure><img src={rev.img} alt="Shoes" className='w-32 h-32 rounded-full mb-[-25px] z-50'/></figure>
-                                <div className="card-body bg-info">
-                                    <h2 className="card-title">{rev.name}</h2>
-                                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                                    <div className="card-actions justify-end">
-                                        <button className="btn btn-primary">Buy Now</button>
+                            <div className="hero border-l-8 border-l-indigo-500">
+                                <div className="hero-content flex-col lg:flex-row-reverse">
+                                    <img src={rev.img} className="max-w-sm rounded-lg shadow-2xl" />
+                                    <div>
+                                        <p className="pb-6"><span className='font-black text-5xl'>"</span>Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                                        <div className='ml-5'>
+                                            <p className="font-semibold">- {rev.name}</p>
+                                            <p className='py-3'>{rev.designation}</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </SwiperSlide>
                     ))
                 }
-            </Swiper>
-        </div>
+
+            </Swiper >
+        </div >
     );
 };
 
